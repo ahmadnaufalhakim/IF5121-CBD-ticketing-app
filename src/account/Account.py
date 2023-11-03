a9d8c749d26df0902fa96372130498d455ee1f5d
I Putu Pawesi Siantika, S.T <pawesisiantika98@gmail.com>
2023-11-03 10:12:30 +0700

    Added admin and user login process

--- src/account/Account.py
+++ src/account/Account.py
@@ -4,16 +4,11 @@ from abc import abstractmethod, ABC
 abs_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 
 class Account:
-    """ Account """
-    """ user credentials"""
-    user_credentials = {
-        'sian@gmail.com' : 'admin123',
-        'adit@zetta.sg' : '123456'
-        }
-    
+
     def __init__(self) -> None:
         self._email = None 
         self._password = None
+        self._database = DictDatabase()
     
     @property
     def email(self):
@@ -30,33 +25,31 @@ class Account:
     @password.setter
     def password(self, user_password):
         self._password = user_password
-
+      
     def login(self):
-        if self._email in self.user_credentials and \
-            self._password == self.user_credentials[self._email]:
-            return self._email
-        else:
-            return False
+        pass
 
-    def reset_password(self, email, new_password) -> bool:
-        if email in self.user_credentials:
-            self.user_credentials[email] = new_password
-            return True
-        else:
-            return False
+    def reset_password(self):
+        pass
 
 
 class User(Account):
 
-    def __init__(self):
+    def __init__(self)-> None:
         super().__init__()
         self._membership_status = None
+    
+    def login(self):
+        return {self.email : self._database.get_user_credentials() [self.email]} if self.email \
+            in self._database.get_user_credentials() and self._database.get_user_credentials() \
+                [self.email]['password'] == self.password else False
 
     def register_membership(self):
-        pass 
+        print("bayar")
 
     def is_member(self) -> bool:
-        pass 
+        return True if self._database.get_user_credentials()[self.email]['membership_status']\
+           != None else  False
 
 
 class Admin(Account):
@@ -64,19 +57,68 @@ class Admin(Account):
     def __init__(self):
         super().__init__()
         self._admin_number = None 
-    
-    @property
-    def admin_number(self):
-        return self._admin_number
-    
-    @admin_number.setter
-    def admin_number(self, adm_number):
-        self._admin_number = adm_number
+
+    def login(self):
+        if self.email in self._database.get_admin_credentials() and self._database.get_admin_credentials() \
+          [self.email]['password'] == self.password:
+            self._admin_number = self._database.admin_credentials[self.email]['admin_number']
+            return {self.email : self._database.get_admin_credentials()[self.email]}
+        else:
+            return False
+
 
 
 class Database(ABC):
+
     @abstractmethod
     def get_user_credentials(self):
         pass
 
-class PickleDatabase()
+    @abstractmethod
+    def get_admin_credentials(self):
+        pass
+
+
+class DictDatabase(Database):
+    """ Implementation using simpledict data type objects in python 3"""
+    user_credentials = {
+        'sian@gmail.com' : {
+            'password' : 'akuganteng',
+            'role' : 'user',
+            'membership_status' : True,
+            'number' : '001',
+            'expiry_date' : None,
+            'payment_status' : False
+        },
+        'nisa@gmail.com' : {
+            'password' : 'qwery123',
+            'role' : 'user',
+            'membership_status' : None,
+            'number' : '002',
+            'expiry_date' : None,
+            'payment_status' : 'belum'
+        },
+        'dicky@yahoo.com' : {
+            'password' : 'husqivarna',
+            'role' : 'user',
+            'membership_status' : None ,
+            'number' : '003',
+            'expiry_date' : None,
+            'payment_status' : False
+        }
+    }
+
+    admin_credentials = {
+        'evita@studio21.com' : {
+            'password' : 'lexus231',
+            'role' : 'admin',
+            'admin_number' : '001',
+        },
+    }
+
+    def get_user_credentials(self):
+        return self.user_credentials
+    
+    def get_admin_credentials(self):
+        return self.admin_credentials
+        
--- src/account/example.py
+++ src/account/example.py
@@ -10,8 +10,15 @@ if user_selection == '1':
 
 elif user_selection == '2':
     account = Admin()
+    account.email = input("Masukkan email: ")
+    account.password = input("Masukkan password: ")
+    print(account.login())
 
 elif user_selection == '3':
     account = User()
     account.reset_password("sian@gmail.com", "akuganteng")
     print(Account.user_credentials)
+    exit(0)
+
+else:
+    print("pilihan tidak ditemukan")
